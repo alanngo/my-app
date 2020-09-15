ac6c77b30dc98fc275bc8f82cea5045a3b2ff584
Alan <alan@remote.com>
2020-09-15 13:41:21 -0500

    everything except laptop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, CanActivate} from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
 import { RootComponent } from './root/root.component';
 import { TodoComponent } from './todo/todo.component';
 import { CommandsComponent } from './commands/commands.component';
 import { RoutingComponent } from './routing/routing.component';
 import { LoginComponent } from './login/login.component';
 import { LaptopComponent } from './laptop/laptop.component';
-import { AuthGuard } from './auth.guard';
 import { DeadEndComponent } from './dead-end/dead-end.component';
 import { BindingComponent } from './binding/binding.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { AuthGuard } from './auth.guard';
+import { StudentsComponent } from './students/students.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 const routes: Routes = 
 [
@@ -19,7 +22,10 @@ const routes: Routes =
     {path:'login', component: LoginComponent},
     {path:'deadEnd', component: DeadEndComponent},
     {path: 'binding', component:BindingComponent},
-    {path:'laptop', component: LaptopComponent, canActivate: [AuthGuard]},
+    {path:'laptop', component: LaptopComponent, canActivate: [AuthGuard]}, 
+    {path: 'directives', component:DirectivesComponent},
+    {path: 'students', component: StudentsComponent},
+    {path: 'pipes', component: PipesComponent},
 
     { path: '', redirectTo: '/root', pathMatch: 'full' }, //default route
     { path: '**', redirectTo: '/deadEnd', pathMatch: 'full' } //catch all route
